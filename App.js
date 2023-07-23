24ac30af0d4e9b510c52879cf5c430505e6c7e42
MeyssaMsolli <125616780+MeyssaMsolli@users.noreply.github.com>
2023-07-23 03:43:45 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,202 @@
+import logo from "./image/favicon.jpg";
+import profilePoject from "./image/favicon.jpg";
+import comingSoon from"./image/coming-soon.png";
+// import getHub from"./image/github.png";
+// import linkedIn from"./image/linked in.png";
+// import{Link} from"https://www.linkedin.com/in/meyssa-msolli-a494081b1/"
+import "./App.css";
+// import CustomTitle from "./components/CustomTitle";
+// import CustomCard from "./components/CustomCard";
+
+function App() {
+return (
+    <div className="App">
+    <header className="App-header">
+    {/* <CustomTitle > */}
+        {/* <img src={profilePoject}/> */}
+        <nav>
+        <h1>Portfolio</h1>
+        <div class="portfolio">
+            <a 
+            href="./App.js"
+            >
+            About
+            </a>
+            <a
+            href="./App.js"
+            >
+            Projects
+            </a>
+            <a href="./App.js"
+            >
+            Contact
+            </a>
+        </div>
+    </nav>
+    <h1 
+    id="title"
+    >
+    Projects
+    </h1>
+    {/* </CustomTitle> */}
+    
+    </header>
+
+<body>
+
+    <div class="projects">
+
+        <div class="project">
+            <img src= {comingSoon} width="300px" height="300px" alt="coming-soon" />
+            <h3>wistia interface replication</h3>
+            <p class="text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt
+                blanditiis id nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt blanditiis id
+                nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+            </p>
+            <div class="select">
+                <div class="demo">
+                    <a href="./app.js">Demo</a>
+
+                </div>
+                <div class="code">
+                    <a href="app.js">Code</a>
+                </div>
+                <h6>Html & css</h6>
+            </div>
+
+        </div>
+
+        <div class="project">
+            <img src={comingSoon} width="300px" height="300px" alt="coming-soon" />
+            <h3>wistia interface replication</h3>
+            <p class="text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt
+                blanditiis id nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt blanditiis id
+                nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+            </p>
+            <div class="select">
+                <div class="demo">
+                    <a href="./app.js">Demo</a>
+
+                </div>
+                <div class="code">
+                    <a href="app.js">Code</a>
+                </div>
+                <h6>Html & css</h6>
+            </div>
+        </div>
+
+        <div class="project">
+            <img src={comingSoon} width="300px" height="300px" alt="coming-soon" />
+            <h3>wistia interface replication</h3>
+            <p class="text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt
+                blanditiis id nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt blanditiis id
+                nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+            </p>
+            <div class="select">
+                <div class="demo">
+                    <a href="./app.js">Demo</a>
+
+                </div>
+                <div class="code">
+                    <a href="app.js">Code</a>
+                </div>
+                <h6>Html & css</h6>
+            </div>
+        </div>
+
+        <div class="project">
+            <img src={comingSoon} width="300px" height="300px" alt="coming-soon" />
+            <h3>wistia interface replication</h3>
+            <p class="text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt
+                blanditiis id nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt blanditiis id
+                nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+            </p>
+            <div class="select">
+                <div class="demo">
+                    <a href="./app.js">Demo</a>
+
+                </div>
+                <div class="code">
+                    <a href="app.js">Code</a>
+                </div>
+                <h6>Html & css</h6>
+            </div>
+        </div>
+
+        <div class="project">
+            <img src={comingSoon} width="300px" height="300px" alt="coming-soon" />
+            <h3>wistia interface replication</h3>
+            <p class="text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt
+                blanditiis id nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt blanditiis id
+                nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+            </p>
+            <div class="select">
+                <div class="demo">
+                    <a href="./app.js">Demo</a>
+
+                </div>
+                <div class="code">
+                    <a href="app.js">Code</a>
+                </div>
+                <h6>Html & css</h6>
+            </div>
+        </div>
+
+        <div class="project">
+            <img src={comingSoon} width="300px" height="300px" alt="coming-soon" />
+            <h3>wistia interface replication</h3>
+            <p class="text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt
+                blanditiis id nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quidem suscipit, incidunt blanditiis id
+                nisi
+                minima vitae perferendis voluptas facere aut nulla. A aliquid dolor incidunt minima quidem ut eveniet.
+            </p>
+            <div class="select">
+                <div class="demo">
+                    <a href="./app.js">Demo</a>
+
+                </div>
+                <div class="code">
+                    <a href="app.js">Code</a>
+                </div>
+                <h6>Html & css</h6>
+            </div>
+        </div>
+    </div>
+    </body>
+{/* <div class="contacts">
+        <h1>Contact Me</h1>
+        <h2>Meyssa Msolli</h2>
+        <div class="contact">
+            <h2>20536244</h2>
+        <h2>designed by Meyssa</h2>
+        </div>
+                <div class="image">
+            <a className="imog" href="https://github.com/" target="_blank" src={getHub} alt="" width="100px"></a>
+            <a className="imog" href={Link} target="_blank" src={linkedIn} alt="" 
+                width="100px"/>
+            <a className="imog" href="https://www.codewars.com/" target="_blank"><img src="./res/codewars-512.webp" alt=""
+                    width="100px"/></a>
+            </div>
+        <h2>meyssa1992@gmail.com</h2>
+        </div> */}
+    </div>
+);
+}
+export default App;
